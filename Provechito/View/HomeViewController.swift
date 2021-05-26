7757247a00c8a3667721cb34257e2514a62a4c0d
Carlos Martínez <cmtz.rdz.98@gmail.com>
2021-05-25 20:25:24 -0500

    Boiler plate
    - Basic layout of views
    - Project structure
    - Basic controllers

---
+++ Provechito/View/HomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeController.swift
+//  Provechito
+//
+//  Created by Carlos Martínez Rodríguez on 23/05/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension HomeViewController : UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width / 2.5, height: collectionView.frame.width / 2.5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecipeCardCell", for: indexPath)
+        return cell
+    }
+}
+
+class RecipeCardCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
