ee74018d8a1d902f59a53280927efbae9b74cf5d
Diego Palmerin <57266493+Dpalme@users.noreply.github.com>
2021-05-25 21:31:33 -0700

    basic styles, poppins, colors and splash

---
+++ Provechito/View/RoundButton.swift
@@ -0,0 +1,13 @@
+//
+//  RoundButton.swift
+//  Provechito
+//
+//  Created by user189093 on 5/25/21.
+//
+
+import Foundation
+import UIKit
+
+class RoundButton: UIButton {
+    override var layer: CALayer
+}
